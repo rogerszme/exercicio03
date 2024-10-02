80136e9d098a48772b18f18663c64b9abd3c167a
Rogério Gomes <rogerszme@hotmail.com>
2024-10-02 20:24:54 -0300

    Add files via upload
    a = 10
    b = 20
    print('A soma dos números é:',(a+b))
    nome = 'Alan'
    sobrenome = 'Maniel'
    print('O nome completo é:', (nome)+' '+ (sobrenome))
    nota1 = float(input(" Digite a Primeira Nota: "))
    nota2 = float(input(" Digite a Segunda Nota: "))
    nota3 = float(input(" Digite a Tercera Nota: "))
    media = (nota1 + nota2 + nota3) / 3
    print('A media das 3 notas é:', (media))
    largura = float(input(" Digite a Largura: "))
    altura = float(input(" Digite a Area:"))
    print('A Área do Triângulo é:',(largura*altura))

---
+++ exercicio03.py
@@ -0,0 +1,14 @@
+a = 10
+b = 20
+print('A soma dos números é:',(a+b))
+nome = 'Alan'
+sobrenome = 'Maniel'
+print('O nome completo é:', (nome)+' '+ (sobrenome))
+nota1 = float(input(" Digite a Primeira Nota: "))
+nota2 = float(input(" Digite a Segunda Nota: "))
+nota3 = float(input(" Digite a Tercera Nota: "))
+media = (nota1 + nota2 + nota3) / 3
+print('A media das 3 notas é:', (media))
+largura = float(input(" Digite a Largura: "))
+altura = float(input(" Digite a Area:"))
+print('A Área do Triângulo é:',(largura*altura))
